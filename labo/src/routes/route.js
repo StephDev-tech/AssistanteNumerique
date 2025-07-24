02f9aec59b8d3588a12dd483c5830c1d76e2db83
OPBS Liyana <liyana.perso@gmail.com>
2025-07-23 22:25:19 -0400

    fix: correction du bug url v3

--- labo/src/routes/route.js
+++ labo/src/routes/route.js
@@ -1,14 +1,14 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HelloWorld from '../components/HelloWorld.vue'
 import Comparateur from '../components/Comparateur.vue'
 
 const routes = [
-  { path: '/', component: HelloWorld ,name: 'Home' },
+  { path: '/', component: HelloWorld ,name: 'Incrementer' },
   { path: '/comparateur', component: Comparateur ,name: 'Comparateur' }
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes
 })
 
